8c91b46e956397169ea058c41fbe6461ba6c5b7f
cxos <cxosoff@gmail.com>
2017-09-14 11:35:57 +0300

    add logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  for (let i = 0; i < str.length; i += 1) {
+    bracketsConfig.forEach((config) => {
+      if (config[1] === str[i]) {
+        if (stack[stack.length - 1] === config[0]) {
+          stack.pop();
+        } else {
+          stack.push(config[1]);
+        }
+      } else if (config[0] === str[i]) {
+        stack.push(config[0]);
+      }
+    });
+  }
+
+  if (stack.length) {
+    return false;
+  }
+
+  return true;
+};
